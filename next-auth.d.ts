174622fc0bd78ac86f943bec537f06284a453c61
lin001234 <162286563+lin001234@users.noreply.github.com>
2026-01-17 20:45:37 +0800

    added connecting to x feature

---
+++ next-auth.d.ts
@@ -0,0 +1,16 @@
+// next-auth.d.ts
+import NextAuth from "next-auth"
+
+declare module "next-auth" {
+  interface Session {
+    accessToken?: string
+    xAccessToken?: string
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    accessToken?: string
+    xAccessToken?: string
+  }
+}
